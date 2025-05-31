aa2940d4d47513532a284349dd699873808d9f6f
mhingan <mhingan6@gamil.com>
2025-05-31 12:17:29 +0300

    update - all, adding comments

--- src/main/java/org/example/ex1_thread/TimerThread.java
+++ src/main/java/org/example/ex1_thread/TimerThread.java
@@ -1,32 +1,41 @@
 package org.example.ex1_thread;
 
+// Clasa TimerThread extinde Thread si reprezinta un fir de executie care masoara timpul in milisecunde
 class TimerThread extends Thread {
+    // Variabila 'running' controleaza daca firul de executie trebuie sa continue
+    // 'volatile' asigura ca modificarile sunt vizibile imediat pentru toate thread-urile
     private volatile boolean running = true;
+
+    // 'elapsed' tine cont de timpul scurs in milisecunde
     private volatile long elapsed = 0;
 
     @Override
     public void run() {
+        // Cat timp 'running' este true, firul continua sa ruleze
         while (running) {
             try {
-                Thread.sleep(1);      // dormim 1 ms
-                elapsed++;            // incrementăm contorul
+                Thread.sleep(1);  // opreste executia pentru 1 milisecunda
+                elapsed++;        // incrementeaza timpul scurs cu 1 ms
             } catch (InterruptedException e) {
+                // daca firul este intrerupt, se opreste setand running pe false
                 running = false;
             }
         }
     }
 
+    // Returneaza timpul scurs in milisecunde
     public long getElapsed() {
         return elapsed;
     }
 
+    // Reseteaza timpul scurs la 0
     public void reset() {
         elapsed = 0;
     }
 
+    // Opreste firul de executie setand running pe false si il intrerupe daca este adormit
     public void shutdown() {
         running = false;
-        this.interrupt();
+        this.interrupt(); // forteaza iesirea din sleep
     }
 }
-
---
+++ src/main/java/org/example/ex3_thread/MyExcept.java
@@ -0,0 +1,7 @@
+package org.example.ex3_thread;
+
+public class MyExcept extends Throwable {
+    public MyExcept(String message) {
+        super(message);
+    }
+}
